a6fac454ce5bf3cf15e8c1b51dcc4a9b1b4138a0
LeonardoV7 <leonardov7@gmail.com>
2021-11-09 07:51:21 -0500

    Carga de archivos

---
+++ lib/Registro.dart
@@ -0,0 +1,108 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Registro extends StatefulWidget {
+  @override
+  RegistroApp createState() => RegistroApp();
+}
+class RegistroApp extends State<Registro>{
+  TextEditingController correo = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  TextEditingController nombre = TextEditingController();
+  TextEditingController documento = TextEditingController();
+  final firebase=FirebaseFirestore.instance;
+  create() async{
+    try{
+      await firebase
+          .collection("Usuarios")
+          .doc()
+          .set({
+          "nombre":nombre.text,
+          "Documento":documento.text,
+        "Correo":correo.text,
+        "Password":pass.text
+      });
+    }catch(e){
+      print(e);
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Registro de usuarios"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+            children: [
+              Padding(
+                padding:
+                EdgeInsets.only(left: 15.0, top: 0, right: 15, bottom: 10),
+                child: TextField(
+                  controller: nombre,
+                  decoration: InputDecoration(
+                      labelText: "Nombre",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                ),
+              ),
+              Padding(
+                padding:
+                EdgeInsets.only(left: 15.0, top: 0, right: 15, bottom: 10),
+                child: TextField(
+                 controller: documento,
+                  decoration: InputDecoration(
+                      labelText: "Documento",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                ),
+              ),
+              Padding(
+                padding:
+                EdgeInsets.only(left: 15.0, top: 0, right: 15, bottom: 10),
+                child: TextField(
+                  controller: correo,
+                  decoration: InputDecoration(
+                      labelText: "Correo",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                ),
+              ),
+              Padding(
+                padding:
+                EdgeInsets.only(left: 15.0, top: 0, right: 15, bottom: 10),
+                child: TextField(
+                  controller: pass,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                ),
+              ),
+              Row(
+                children: [
+
+                  ElevatedButton(
+                      style: TextButton.styleFrom(backgroundColor: Colors.blueGrey),
+                      onPressed: (){
+                       create();
+                        nombre.clear();
+                        documento.clear();
+                        correo.clear();
+                        pass.clear();
+                      },
+                      child: Text("Enviar"))
+                ],
+              )
+            ]
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'Registro.dart';
+import 'Inicio.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Login());
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  LoginStart createState() => LoginStart();
+}
+
+class LoginStart extends State<Login> {
+  TextEditingController correo = TextEditingController();
+  TextEditingController pass = TextEditingController();
+
+  validarUser() async {
+    try {
+      CollectionReference referencia =
+          FirebaseFirestore.instance.collection("Usuarios");
+      QuerySnapshot usuario = await referencia.get();
+
+      if (usuario.docs.length != 0) {
+        for (var cursor in usuario.docs) {
+         // print(cursor.get("Correo"));
+          if (cursor.get("Correo") == correo.text) {// validación de correo
+            print("Correo encontrado -- " + cursor.get("nombre"));
+            if (cursor.get("Password") == pass.text) {// validación de password
+              Navigator.push(context, MaterialPageRoute(builder: (_)=> Inicio()));
+            } else {
+              print("usuario incorrecto");
+            }
+          } else {
+            //print("Correo no encontrado");
+          }
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Formulario Login"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset('assets/login_1.png'),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, top: 0, right: 15, bottom: 10),
+              child: TextField(
+                controller: correo,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Correo',
+                  hintText: 'Digite Correo',
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 10),
+              child: TextField(
+                controller: pass,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Digite su contraseña',
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 50,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: FlatButton(
+                      onPressed: () {
+                        validarUser();
+                      },
+                      child: Text(
+                        'Enviar',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                ),
+                Container(
+                  height: 50,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: FlatButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => Registro()));
+                      },
+                      child: Text(
+                        'Registro',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
